1b1eaced26ffb13bb95d5351afc26a09387c6ed0
Aaron Zuercher <aazuercher@gmail.com>
2025-05-17 16:44:56 -0500

    restructure directories

---
+++ brand/colors/ZLightStudio_hex_colors.lua
@@ -0,0 +1,23 @@
+##ZLightStudio hex colors##
+
+Radiant Ember - #ff6a00
+Golden Flare - #ffa940
+Soft Amber - #ffbc70
+Sunrize Haze - #ffd7a0
+Honeygold Mist - #ffeac0
+Claylight - #d47a42
+Glowroot Brown - #a55026
+Ash Veil - #f5f5f5
+Fogstone - #e0e0e0
+Graphite Glow - #b0b0b0
+Smokelight Charcoal - #5a5a5a
+Obsidian Fade - #2e2e2e
+Serene Azure - #4a90e2
+Slate Blue - #3d7cb5
+Dusky Teal - #3a9e9f
+Pale Blue Sky - #6baad7
+Soft Mist Blue - #95bfd7
+Coastal Blue - #346c9c
+Ocean Deep Blue - #2b5779
+Black - #000000
+White - #ffffff
